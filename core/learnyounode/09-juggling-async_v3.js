cb1b868a5a0cc8cddb066f9b7f09464095b4bcd0
Sebastian Thoss <thoss_sebastian@web.de>
2015-05-23 12:08:20 +0200

    task 9

--- core/learnyounode/09-juggling-async_v3.js
+++ core/learnyounode/09-juggling-async_v3.js
@@ -2,7 +2,7 @@ var bl = require('bl');
 var http = require('http');
 var async = require('async');
 
-async.series([
+async.parallel([
     function(callback) {
         collectData(process.argv[2], callback);
     },
@@ -11,14 +11,17 @@ async.series([
     },
     function(callback) {
         collectData(process.argv[4], callback);
-    },
-]);
+    }
+], function (err, results) {
+    for (var i = 0; i < results.length; i++) {
+        console.log(results[i]);
+    }
+});
 
 function collectData(url, callback) {
     http.get(url, function(response) {
         response.pipe(bl(function (err, data) {
             var text = data.toString();
-            console.log(text);
             callback(null, text);
         }))
     });

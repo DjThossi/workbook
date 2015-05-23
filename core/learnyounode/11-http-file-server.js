8b3c26b2bf8e638a3981085fe2c4b82105195586
Sebastian Thoss <thoss_sebastian@web.de>
2015-05-23 13:12:54 +0200

    optimized task 11

--- core/learnyounode/11-http-file-server.js
+++ core/learnyounode/11-http-file-server.js
@@ -5,14 +5,12 @@ var fileLocation = process.argv[3];
 
 var server = http.createServer(function (request, response) {
     var readStream = fs.createReadStream(fileLocation);
-    var text = [];
     readStream.on('data', function(chunk) {
-        text.push(chunk);
+        response.write(chunk);
     });
 
     readStream.on('end', function () {
-        text = Buffer.concat(text);
-        response.end(text);
+        response.end();
     });
 });
 
